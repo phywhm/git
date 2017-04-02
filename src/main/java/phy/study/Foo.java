cfdb45a99183b561523202615aad8e51a43ea112
mark.pan <panhaoyang1234@163.com>
2017-04-02 10:43:24 +0800

    sec

---
+++ src/main/java/phy/study/Foo.java
@@ -0,0 +1,11 @@
+package phy.study;
+
+/**
+ * Created by HASEE on 2017/4/2.
+ */
+public class Foo
+{
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
